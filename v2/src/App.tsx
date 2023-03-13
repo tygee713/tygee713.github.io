781cc2a541c35970c5182f624f9488084ce15ee5
Tyler Geerdts <tygee713@gmail.com>
2023-03-13 18:18:54 -0400

    Finished webpack config, dev server rendering

---
+++ v2/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => {
+  return (<div>This is a sample React 18 with typescript compiled using webpack</div>)
+}
+
+export default App
---
+++ v2/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: '[id].[chunkhash].js',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'public/index.html',
+  })]
+}
